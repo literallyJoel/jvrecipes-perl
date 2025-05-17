4671c0c1ebb302fdfcbb99e84a0e7cf79a07d563
Joel Vivian <joel@jdvivian.co.uk>
2025-05-17 17:24:35 +0100

    tree structure

--- lib/JVRecipes/Router/Base.pm
+++ lib/JVRecipes/Router/Base.pm
@@ -5,9 +5,12 @@ use JSON::MaybeXS;
 use Try::Tiny;
 use Plack::Request;
 use Module::Load;
+use JVRecipes::Router::Node;
 
-has "prefix" => ( is => "ro", isa => "Str", default => "");
-has "routes" => ( is => "rw", isa => "ArrayRef", default => sub{[]});
+has "prefix"  => ( is => "ro", isa => "Str", default => "" );
+has "_routes" => ( is => "ro", isa => "JVRecipes::Router::Node", default => sub {
+    JVRecipes::Router::Node->new;
+});
 
 sub group {
     my $self   = shift;
@@ -15,15 +18,18 @@ sub group {
     my $module = shift;
 
     load $module;
+
     die "$module is not a valid router instance"
         unless $module->can("new");
     
     my $router = $module->new(prefix => $self->prefix . $prefix);
 
     die "$router is not a valid router instance"
-         unless $router->can("_router");
-
-    push $self->routes->@*, $router->_router->routes->@*;
+        unless $router->can("_router");
+    
+    for my $method (qw(GET POST PUT DELETE ANY)) {
+        $self->_child_route($router->_router, $method);
+    }
 
     return $self;
 }
@@ -34,6 +40,94 @@ sub put    {shift->_add_route("PUT", @_)}
 sub delete {shift->_add_route("DELETE", @_)}
 sub any    {shift->_add_route("ANY", @_)}
 
+sub handle {
+    my $self = shift;
+    my $env  = shift;
+
+    my $request = Plack::Request->new($env);
+    my $path    = $request->path_info;
+    my $method  = $request->method;
+
+    my @segments;
+    @segments = grep { $_ ne ""} split "/", $path unless $path eq "/";
+    @segments = ("") if $path eq "/";
+
+    my ($controller, $path_params, $has_other_methods) =
+        $self->_routes->find_route(
+            method        => $method,
+            path_segments => \@segments,
+        );
+    
+    return $self->_invoke(
+        controller  => $controller,
+        request     => $request,
+        path_params => $path_params,
+        route_path  => $path,
+    ) if $controller;
+
+    my $response_code = $has_other_methods ? 405 : 404;
+    my $error_message = $has_other_methods ? "Method Not Allowed" : "Not Found";
+
+    return [
+        $response_code,
+        ["Content-Type", "application/json"],
+        [encode_json({errors => [{$response_code => $error_message}]})],
+    ];
+}
+
+sub _child_route {
+    my $self   = shift;
+    my $router = shift;
+    my $method = shift;
+
+    $self->_find_route(
+        node => $router->_routes,
+        method => $method
+    );
+}
+
+sub _find_route {
+        my $self = shift;
+        my %args = @_;
+
+        my $node   = $args{node};
+        my $prefix = $args{prefix} || "";
+        my $method = $args{method};
+
+        if(exists $node->handlers->{$method}) {
+            my $controller = $node->handlers->{$method};
+            my $path = $prefix || "/";
+
+            $self->_add_route_to_tree(
+                method     => $method,
+                path       => $path,
+                controller => $controller
+            );
+        }
+
+        for my $key (keys $node->children->%*) {
+            my $child      = $node->children->{$key};
+            my $new_prefix = $prefix ? "$prefix/$key" : "/$key";
+            $self->_find_route(
+                node   => $child,
+                prefix => $new_prefix,
+                method => $method,
+            );
+        }
+
+        if($node->wildcard) {
+            my $new_prefix = $prefix ? "$prefix/*" : "/*";
+            if (exists $node->wildcard->handlers->{$method}) {
+                my $controller = $node->wildcard->handlers->{$method};
+                $self->_add_route_to_tree(
+                    method     => $method,
+                    path       => $new_prefix, 
+                    controller => $controller
+                );
+            }
+        }
+}
+
 sub _add_route {
     my $self       = shift;
     my $method     = shift;
@@ -41,100 +135,41 @@ sub _add_route {
     my $controller = shift;
 
     load $controller;
-
     die "$controller is not a valid controller instance"
         unless $controller->can("new") && $controller->can("run");
-
-    push $self->routes->@*, [
-        $method,
-        $self->prefix . $path,
-        $controller,
-    ];
+    
+    $self->_add_route_to_tree(
+        method     => $method, 
+        path       => $self->prefix . $path,
+        controller => $controller
+    );
 
     return $self;
 }
 
-sub handle {
+sub _add_route_to_tree {
     my $self = shift;
-    my $env  = shift;
-    
-    my $request = Plack::Request->new($env);
-    my $path    = $request->path_info;
-    my $method  = $request->method;
+    my %args = @_;
 
-    my $wrong_method = 0;
-    
-    for my $route ($self->routes->@*) {
-        my ($route_method, $route_path, $controller) = @$route;
-        
-        die "$controller is not a valid controller instance"
-            unless $controller->can("new") && $controller->can("run");
+    my $method     = $args{method};
+    my $path       = $args{path};
+    my $controller = $args{controller};
 
-        if($route_path eq $path && $route_method ne $method && $route_method ne "ANY") {
-            $wrong_method = 1;
-            next;
-        }
+    my @segments;
+    @segments = grep { $_ ne ""} split "/", $path unless $path eq "/";
+    @segments = ("") if $path eq "/";
 
-        next unless $route_method eq $method || $route_method eq "ANY";
-        
-        return $self->_invoke(
-            controller => $controller,
-            request    => $request,
-            route_path => $route_path
-        ) if $route_path eq $path;
-        
-        if ($route_path =~ /\*$/) {
-            my $prefix = $route_path;
-            $prefix =~ s/\*$//;
-
-            return $self->_invoke(
-                controller => $controller,
-                request    => $request,
-                route_path => $route_path
-            ) if $path =~ /^$prefix/;
-        }
-
-        if ($route_path =~ /:/) {
-            my @route_segments = split "/", $route_path;
-            my @path_segments  = split "/", $path;
-
-            next if @route_segments != @path_segments;
-
-            my $matches = 1;
-
-            my %params;
-
-            for my $i (0..$#route_segments) {
-                if($route_segments[$i] =~ /^:(.+)$/) {
-                    $params{$1} = $path_segments[$i];
-                } elsif ($route_segments[$i] ne $path_segments[$i]) {
-                    $matches = 0;
-                    last;
-                }
-            }
-
-            return $self->_invoke(
-                controller  => $controller,
-                request     => $request,
-                path_params => \%params,
-                route_path  => $route_path,
-            ) if $matches;
-        }
-    }
-
-    my $response_code = $wrong_method ? 405 : 404;
-    my $error_message = $wrong_method ? "Method not allowed" : "Not Found";
-
-    return [
-        $response_code,
-        ["Content-Type" => "application/json"],
-        [encode_json({errors => [{$response_code => $error_message}]})]
-    ];
+    $self->_routes->add_route(
+        method        => $method,
+        path_segments => \@segments,
+        controller    => $controller,
+    );
 }
 
 sub _invoke {
-    my $self        = shift;
-    my %args        = @_;
+    my $self = shift;
+    my %args = @_;
+
     my $controller  = $args{controller};
     my $request     = $args{request};
     my $path_params = $args{path_params} || {};
@@ -146,24 +181,18 @@ sub _invoke {
         my $instance = $controller->new(request => $request, path_params => $path_params);
         $response = $instance->run;
     };
-   
+
     if($@) {
         warn "Error handling request to $route_path: $@";
 
         return [
             500,
             ["Content-Type" => "application/json"],
-            [encode_json({errors => [500 => "Internal Server Error"]})]
+            [encode_json({errors => [{500 => "Internal Server Error"}]})]
         ];
     }
 
     return $response;
-}
-
-sub clear_routes {
-    my $self = shift;
-
-    $self->routes->@* = ();
 }
 
 1;
