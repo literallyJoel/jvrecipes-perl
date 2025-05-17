03f70c1b6dafd57397c3c7c8d5869a83d44b651e
Joel Vivian <joel@jdvivian.co.uk>
2025-05-17 15:22:03 +0100

    initial database handling

---
+++ lib/JVRecipes/Role/Database/Connector.pm
@@ -0,0 +1,21 @@
+package JVRecipes::Role::Database::Connector;
+
+use Mouse;
+
+use JVRecipes::Database::Database;
+
+
+has 'database' => ( is => "ro", isa => "JVRecipes::Database::Database", lazy_build => 1);
+has 'dbh'      => ( is => "ro", isa => "DBI::db", lazy_build => 1);
+
+sub _build_database {
+    return JVRecipes::Database::Database->new;
+}
+
+sub _build_dbh {
+    my $self = shift;
+
+    return $self->database->dbh;
+}
+
+1;
